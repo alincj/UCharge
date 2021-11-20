7f088467fce8a3c7f1442e77a53fe2f90d5c16b1
alincj <alin.cj10@gmail.com>
2021-11-20 20:54:29 +0000

    second

---
+++ lib/screens/logo_and_back.dart
@@ -0,0 +1,37 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_task_planner_app/widgets/back_button.dart';
+
+class Perfil extends StatelessWidget {
+  @override
+  Widget build(BuildContext context){
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: size.height,
+        alignment: Alignment.center,
+        child: Stack( children: <Widget>[
+          Positioned(
+              top: 60,
+              left: 15,
+              child: MyBackButton()
+          ),
+          Positioned(
+              top: 20,
+              right: 0,
+              child: Image.asset(
+            "assets/images/U-Charge_Logo.png",
+            width: size.width*0.3,)
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+
+
+
+
+}
